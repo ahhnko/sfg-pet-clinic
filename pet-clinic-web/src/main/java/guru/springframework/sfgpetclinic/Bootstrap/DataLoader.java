b4bbe6bd5ba2cbfccb68305ac76102e746c7dec9
Andrés Halliday Martínez <ahalliday@asesoftware.com>
2022-08-14 20:04:20 -0500

    adding Spring Config. Closes #23

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/Bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/Bootstrap/DataLoader.java
@@ -1,5 +1,6 @@
 package guru.springframework.sfgpetclinic.Bootstrap;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -7,19 +8,14 @@ import guru.springframework.sfgpetclinic.model.Owner;
 import guru.springframework.sfgpetclinic.model.Vet;
 import guru.springframework.sfgpetclinic.services.OwnerService;
 import guru.springframework.sfgpetclinic.services.VetService;
-import guru.springframework.sfgpetclinic.services.map.OwnerServiceMap;
-import guru.springframework.sfgpetclinic.services.map.VetServiceMap;
 
 @Component
 public class DataLoader implements CommandLineRunner {
 
-	private final OwnerService ownerService;
-	private final VetService vetService;
-
-	public DataLoader() {
-		ownerService = new OwnerServiceMap();
-		vetService = new VetServiceMap();
-	}
+	@Autowired
+	private OwnerService ownerService;
+	@Autowired
+	private VetService vetService;
 
 	@Override
 	public void run(String... args) throws Exception {
